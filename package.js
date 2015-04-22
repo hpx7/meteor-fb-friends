fcc14a1b0559fd7406d54f6b5399aa614800ac9d
Harsh Pandey <hpandey793@gmail.com>
2015-04-21 23:30:37 -0700

    initial commit

---
+++ package.js
@@ -0,0 +1,14 @@
+Package.describe({
+  name: 'hpx7:fb-friends',
+  version: '0.0.1',
+  summary: 'Get a user\'s facebook friends',
+  git: '',
+  documentation: null
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.1.0.2');
+  api.use(['underscore', 'http']);
+  api.addFiles('fb-friends.js');
+  api.export('FriendIDs');
+});
